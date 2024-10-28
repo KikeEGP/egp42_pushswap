379783e0601ddc2ebdbab9945954cafdc3b0b154
Enrique Gil Piqueras <enrgil-p@c2r14s2.42madrid.com>
2024-10-28 14:10:55 +0100

    Writing pseudocode

--- pseudocode.c
+++ pseudocode.c
@@ -6,8 +6,18 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 13:24:20 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/10/28 13:24:22 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/10/28 13:33:42 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+//--- Recibe el stack a con todos los elementos y stack b vacío.
+//	--Comprueba que el stack a contiene enteros y que no hay ninguno duplicado
+check_stack_a()
+{
+	if !(stack_a)
+		wait_for_human;
+	if (stack_a->node_x == stack_a->node_y)
+		ft_printf("Error\n%s", standar_errors);
+	else
+		return (go_ahead);
+}
