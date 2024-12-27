5e2165efada72e24b8d79959f5f2fd8f306f7891
Enrique Gil Piqueras Home <proyectosegp@gmail.com>
2024-12-27 18:55:38 +0100

    Created .h for main functions of push_swap

---
+++ push_swap.h
@@ -0,0 +1,9 @@
+/*PUT A HEADER HERE*/
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include "libft/complete_libft.h"
+/*New function allowed: exit(), defined in stdlib.h, included in libft*/
+
+#endif
