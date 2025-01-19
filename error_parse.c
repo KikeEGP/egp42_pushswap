d209be22bbef0546856fa03b88074c1ec3149383
Enrique Gil Piqueras <enrgil-p@c2r8s1.42madrid.com>
2025-01-19 19:59:36 +0100

    Error_parse just for error, check overflow now is in parse_conv_comp

--- error_parse.c
+++ error_parse.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 23:20:18 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/01/19 17:09:33 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/01/19 19:59:06 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,3 @@ void error_argument()
 	ft_putendl("Error");
 	exit(EXIT_FAILURE);
 }
-
-void	check_atoi_overflow(long result, int count)
-{
-	if(!count || (count > 10 && (result == INT_MAX || result == INT_MIN)))
-		error_argument(/*wHAT?*/);
-}
-
