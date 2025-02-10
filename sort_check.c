e77a2f16d9b5fb655ff69cc320b81ae7724cdfad
Enrique Gil Piqueras Home <proyectosegp@gmail.com>
2025-02-10 23:47:58 +0100

    UPDATE IN 42 WITH DEFINITION.H. Created two files to start thinking abput them.

---
+++ sort_check.c
@@ -0,0 +1,14 @@
+/*	*	*	*	*/
+/*				*/
+/*	Include header 42	*/
+/*				*/
+/*	*	*	*	*/
+
+#include "push_swap.h"
+
+/*Function that checks if every node is in expected position*/
+/*It could iterate and compare iteration index with t_stack->position*/
+/*And storage number of times a number is not in expected place*/
+/*Maybe, in case of few unexpected positions, 
+ * could work with different instructions. But pdel-olm suggest to focus
+ * in algorithm. In case of being well programmed, it should do this too*/
---
+++ sort_few.c
@@ -0,0 +1,11 @@
+/*	*	*	*	*/
+/*				*/
+/*	Include header 42	*/
+/*				*/
+/*	*	*	*	*/
+
+#include "push_swap.h"
+
+/*Place to create functions for arguments <= 3, 4 or 5. 
+ *
+ * Remember, max 2 movs with 3 values. 12 moves max in cas of 5*/
