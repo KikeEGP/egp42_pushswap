abc6569faec439dc4878129d9a490ca40e507aef
Enrique Gil Piqueras Home <proyectosegp@gmail.com>
2025-02-10 22:16:40 +0100

    Add a note. UPDATE THIS IN 42 TO ADD A HEADER

--- definitions.h
+++ definitions.h
@@ -9,3 +9,8 @@ typedef struct	s_stack
 	struct s_stack	*next;
 	int		position;
 }	t_stack;
+
+/*pdel-olm storages data in different structs, as matrioshkas
+ * And have two pointers for both stacks: two tops and two bottoms
+ * With top->next and bottom->prev, he can manage easily four values/positions+
+ * KEEP THIS IN MIND AND UPDATE THIS FILE*/
