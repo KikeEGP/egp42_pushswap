86471aed05d4296199ef41447ba229f0c22835e8
42 enrgil-p <enrgil-p@c3r14s1.42madrid.com>
2025-02-11 20:23:00 +0100

    Little idea for two or three arguments to sort

--- sort_few.c
+++ sort_few.c
@@ -6,6 +6,17 @@
 
 #include "push_swap.h"
 
+/*	???	*/	sort_three()/*Try to apply to two, too.
+					And you enter here if (!sort_check)*/
+{
+	if (stack_a->value > /*Bottom->value*/)
+		ra;
+	else
+	{
+		if (stack_a->value >stack_a->next->value)
+			sa;
+	}
+}
 /*Place to create functions for arguments <= 3, 4 or 5. 
  *
  * Remember, max 2 movs with 3 values. 12 moves max in cas of 5*/
