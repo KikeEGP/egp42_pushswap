b5d3607c2dabd7f2d5217be6aa601d7a6edcae97
HOME Enrique Gil Piqueras <proyectosegp@gmail.com>
2025-02-15 20:45:09 +0100

    sort_check first idea

--- sort_check.c
+++ sort_check.c
@@ -18,3 +18,34 @@
 /*Maybe, in case of few unexpected positions, 
  * could work with different instructions. But pdel-olm suggest to focus
  * in algorithm. In case of being well programmed, it should do this too*/
+int	sort_check(t_stack stack_a)
+{
+	/*PARAMETER SHOULD BE A STRUCT WITH BOTH STACKS,
+	 * so you can check if stack_b->value != NULL*/
+	int	wrong_place;
+	/*This var wrong_place should be in struct I mentioned above,
+	 * and could be used to know how many erros are there*/
+	size_t	index;
+	int	check;
+	t_stack	pointer_stack;
+
+	if (!stack_a)
+		return (0);/*MAYBE NOT NECESSARY. OR MAYBE PUT HERE ERROR*/
+	pointer_stack = stack_a;
+	index = 0;
+	wrong_place = 0;
+	check = 1;
+	while (pointer_stack != NULL)
+	{
+		if (check && index != stack_a->position)
+			check = 0;
+		else if (index != stack_a->position)
+
+		index++;
+		pointer_stack = pointer_stack->next;
+	}
+	return (check);
+}
+/*OTHER IDEA: CHECK LIKE STRTRIM. You can use this function from top
+ * and from bottom too. So you can see where is first error, maybe you choose one rotate or other...
+ * BUT REMEMBER IS EASIER TO POP AND PUSH FIRST VALUES*/
