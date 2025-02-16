9ad6c9068208c71046cc183033eac02def18d50f
42 enrgil-p <enrgil-p@c2r19s2.42madrid.com>
2025-02-16 15:20:25 +0100

    Cleaning main

--- main.c
+++ main.c
@@ -6,50 +6,28 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 19:24:20 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/01/18 22:12:55 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/02/16 15:20:06 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/*OKAY, LET'S SEE HOW THIS WORKS*/
-int	main(int arg_counter, char *arg_vector[])
+/*	????	*/	initialize_stack(t_stack /*struct*/)
 {
-	static int	a;
-	int	*p;
+	/*struct*/->value = 0;
+	/*struct*/->prev = NULL;
+	/*struct*/->next = NULL;
+	/*struct*/->position = 0;
+}
 
-	p = &a;
-	ft_printf("Sizeof(int) == %d\n", sizeof(int));
-	ft_printf("Sizeof(long) == %d\n", sizeof(long));
-	//exit(1);
-	ft_printf("p %p, %x\n", p, p);
-	ft_printf("a %d\n", a);
-	ft_printf("&p %p, %x\n", &p, &p);
-	if (arg_counter == 1)
+int	main(int a_counter, char *a_vector[])
+{
+	t_stack	/*struct*/;
+
+	initialize(/*struct*/)
+	if (a_counter == 1)
 		exit(EXIT_FAILURE);/*In case of no arguments, nothing.*/
-
-	/*In case of 2 or more arguments, you may convert to int*/
-
-	/*PARSE: TARGETS
-	 * 	1. Check arguments
-	 * 	2. Create stack_a (and stack_b empty but with same size)
-	 * 	3. Fill stack_a with integers parsed
-	 * 		3.1: in case of error, free, "Error\n" and exit(1)*/	
-	
-	/*Also, you must check if are correct arguments:
-	 * ---integers---
-	 * ---not repetead---
-	 * ---not exceed integer value---*/
-	
-	/*IN CASE OF ERROR, printf or putendl "Error\n"*/
-	
-	/*You must include integers in stack_a. Maybe you can
-	 * parse first arguments to check if are correct, and
-	 * later create stack_a with malloc, knowing number of
-	 * arguments. Remember: LIFO*/
-	if (arg_counter == 2)
-		ft_printf("Hello %s", arg_vector[1]);
-	
+	/*struct????*/ = parse_main(a_counter, a_vector/*, MAYBE SOMETHING*/);
 	/*You have here the stack_a ready. Let's sort it*/
 	
 	/*EXPECTED RESULT IS A LIST OF COMMANDS PRINT*/
