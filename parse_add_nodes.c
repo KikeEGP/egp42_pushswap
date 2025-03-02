85e9626d0f3934ca4b83ded2153e0c69cd2224e9
42 enrgil-p <enrgil-p@c1r9s3.42madrid.com>
2025-03-02 20:20:15 +0100

    Little change

--- parse_add_nodes.c
+++ parse_add_nodes.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 03:40:56 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/03/02 00:11:01 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/03/02 20:19:56 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	add_to_list(int value, t_stack parse_list)
 {
 	t_stack	new_node;
 	new_node = create_node(value);/*THIS CREATES NEW NODE, NOT LIST*/
-	if (!new_node || !add_front(parse_list, new_node)
+	if (!new_node || !add_front(parse_list, new_node))
 		return (0);	
 	return (1);
 }
