887f47f16ab6ecca8ed1e2050d51abf567254d36
42 enrgil-p <enrgil-p@c1r16s3.42madrid.com>
2025-03-02 21:24:07 +0100

    Cleaned with vicmarti. Maybe does not work, but know doesn't return error when compiles

--- parse_add_nodes.c
+++ parse_add_nodes.c
@@ -6,17 +6,17 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 03:40:56 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/03/02 20:19:56 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/03/02 20:39:16 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swaop.c"
+#include "push_swap.h"
 
 t_stack	*create_node(void *content)
 {
-	t_list	*new_node;
+	t_stack	*new_node;
 
-	new_node = (t_list *)malloc(sizeof(new_node));
+	new_node = (t_stack *)malloc(sizeof(new_node));
 	if (!new_node)
 		return (NULL);
 	new_node->value = content;
@@ -26,18 +26,18 @@ t_stack	*create_node(void *content)
 	return (new_node);
 }
 
-int	add_front(t_stack **head, t_stack *new)
+int	add_front(t_stack *head, t_stack *new)
 {
 	if ((head == NULL || new == NULL) || head->value == new->value)
 		return (0);/*Any time is accurate to compare two integers*/
 	head->prev = new;/*Check this functions*/
-	new->next = *head;
-	*head = new;
+	new->next = head;/*This had *head, and was wrong*/
+	*head = *new;/*before was *head = new, and was wrong*/
 }
 
-int	add_to_list(int value, t_stack parse_list)
+int	add_to_list(int value, t_stack *parse_list)
 {
-	t_stack	new_node;
+	t_stack	*new_node;
 	new_node = create_node(value);/*THIS CREATES NEW NODE, NOT LIST*/
 	if (!new_node || !add_front(parse_list, new_node))
 		return (0);	
