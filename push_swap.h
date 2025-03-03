c4848cc5e7dbca056bf97e9e2f0a09342acc2f6f
42 enrgil-p <enrgil-p@c1r10s6.42madrid.com>
2025-03-03 19:02:44 +0100

    Updated push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 19:24:53 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/02/08 21:00:42 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/03/03 18:55:59 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,20 @@
 /*		Defines and structs		*/
 # include "definitions.h"
 
-/*				parse				*/
-int	parse_chars(int arg_counter, char **arg_vector);
-void	error_argument(/*check this one*/);
-char	*get_endptr(const char *str);
-void	check_atoi_overflow(long result, int count);
+/*	*	*	*	* parse	*	*	*	*	*/
+t_stack		*parse_main(int argc, char **argv, /**/);
+int		parse_chars(int arg_counter, char **arg_vector);
+static int	split_to_convert(int argc, char **argv, t_storage stacks);
+static int	convert_and_add(char **split_arg, t_storage stacks);
+static int	string_to_int(const char *source_str, char **endptr);
+/*	* parse_conv_add *	*/
+int		check_atoi_overflow(long result, int count);
+char		*get_endptr(const char *str);
+/*	* parse_add_nodes *	*/
+int		add_to_list(int value, t_stack *parse_list);
+t_stack		*create_node(void *content);
+void		error_argument(/*check this one*/);
+char		*get_endptr(const char *str);
+int		check_atoi_overflow(long result, int count);
 
 #endif
