d998f821369f48261cb48396e68d08929f45cbbb
42 enrgil-p <enrgil-p@c1r2s5.42madrid.com>
2025-03-14 20:33:18 +0100

    Changed initialize with pmendez-. Solution seems to be a **new_stack as parameter, oinstead of *new_node. And declare vars like this way: (*new_node)->name_of_var

--- main.c
+++ main.c
@@ -6,21 +6,21 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 19:24:20 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/03/14 19:16:59 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/03/14 20:29:47 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	initialize_stack(t_stack *new_stack)
+int	initialize_stack(t_stack **new_stack)
 {
-	new_stack = (t_stack *)malloc(sizeof(t_stack));
+	*new_stack = (t_stack *)malloc(sizeof(t_stack));
 	if (!new_stack)
 		return (0);
-	new_stack->value = 0;
-	new_stack->prev = NULL;
-	new_stack->next = NULL;
-	new_stack->position = 0;
+	(*new_stack)->value = 0;
+	(*new_stack)->prev = NULL;
+	(*new_stack)->next = NULL;
+	(*new_stack)->position = 0;
 	return (1);
 }
 
@@ -31,7 +31,7 @@ int	main(int argc, char *a_vector[])
 	if (argc == 1)
 		return (0);
 	if (!initialize_stack(&stack_a)
-		&& !parse_main(argc, a_vector, &stack_a));
+		&& !parse_main(argc, a_vector, stack_a))
 		exit(EXIT_FAILURE);
 	/*You have here the stack_a ready. Let's sort it*/
 	
