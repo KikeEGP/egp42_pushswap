024b2e3a7b22c49b03e3f565e11ec826b7dc491c
42 enrgil-p <enrgil-p@c1r2s5.42madrid.com>
2025-03-14 21:13:07 +0100

    Now endptr is not const. But beware., strange norminette error

--- parse_conversion.c
+++ parse_conversion.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 19:42:00 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/03/12 21:30:11 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/03/14 21:12:08 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ int	check_atoi_overflow(long result, int count)
 }
 
 /*If string != totally converted, returns a pointer*/
-char	*get_endptr(const char *str)
+char	*get_endptr(char *str)
 {
-	const char	*expected_null;
-	int		i;
+	char	*expected_null;
+	int	i;
 
 	expected_null = str;
 	i = 0;
@@ -39,5 +39,5 @@ char	*get_endptr(const char *str)
 		expected_null = &str[i];
 		i++;
 	}
-	return ((char *)expected_null);
+	return (expected_null);
 }
