d89137bacd3563e5a7670808e938bf27cc162d5b
42 enrgil-p <enrgil-p@c1r2s5.42madrid.com>
2025-03-14 21:55:02 +0100

    add_to_list receives a **parse_list

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 19:24:53 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/03/14 21:51:07 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/03/14 21:54:32 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int		parse_chars(int arg_counter, char **arg_vector);
 int		check_atoi_overflow(long result, int count);
 char		*get_endptr(char *str);
 /*	* parse_add_nodes *	*/
-int		add_to_list(int value, t_stack *parse_list);
+int		add_to_list(int value, t_stack **parse_list);
 t_stack		*create_node(void *content);
 void		error_argument(/*check this one*/);
 char		*get_endptr(char *str);
