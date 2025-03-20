cbc94aeea570c10a8a93d536dd45cd36f81e8301
42 enrgil-p <enrgil-p@c2r11s4.42madrid.com>
2025-03-20 21:31:00 +0100

    Find the leak first, pls. Meanwhile, created file for next step to parse

---
+++ parse_check_duplicate.c
@@ -0,0 +1,29 @@
+/* *********************parse_check_duplicate***************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_check_duplicate.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 20:43:53 by enrgil-p          #+#    #+#             */
+/*   Updated: 2025/03/20 21:29:51 by enrgil-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	comparison(t_stack ??parse_list, t_stack **iterator)
+{
+}
+
+int	check_duplicates(t_stack ??parse_list, t_stack ??stack_a)
+{
+	t_stack	*iterator;
+
+	iterator = ??parse_list->next;
+	while (iterator)
+	{
+		if (!comparison(parse_list, &iterator))
+	}
+	//Return 1 in case of no dupplicates. 0 in case of duplicate
+}
