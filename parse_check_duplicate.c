0140fa44b8f370856da40fed216df46665b35954
42 enrgil-p <enrgil-p@c1r10s1.42madrid.com>
2025-03-23 21:22:58 +0100

    norminette in check_duplicates is okay. May the code be okay too?

--- parse_check_duplicate.c
+++ parse_check_duplicate.c
@@ -1,30 +1,44 @@
-/* *********************parse_check_duplicate***************************************************** */
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   parse_check_duplicate.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/20 20:43:53 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/03/23 00:10:07 by enrgil-p         ###   ########.fr       */
+/*   Created: 2025/03/23 21:14:07 by enrgil-p          #+#    #+#             */
+/*   Updated: 2025/03/23 21:18:57 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	comparison(t_stack ??parse_list, t_stack **iterator)
+static int	comparison_values(t_stack ***parse_list, t_stack **iterator)
 {
+	if ((*parse_list)->value == iterator->value)
+		return (0);
+	if ((*parse_list)->value > iterator->value)
+		(*parse_list)->position++;
+	else if ((*parse_list)->value < iterator->value)
+		iterator->position++;
+	return (1);
 }
 
 int	check_duplicates(t_stack **parse_list, t_stack **stack_a)
-{/*
+{
+	int		parse_list_size;
 	t_stack	*iterator;
 
-	iterator = ??parse_list->next;
-	while (iterator)
+	parse_list_size = stack_size(parse_list);
+	while (parse_list_size-- >= 1)
 	{
-		if (!comparison(parse_list, &iterator))
-	}THIS PART SECOND, FIRST WORK IN CHANGE FROM ONE LIST TO OTHER*/
-	//Return 1 in case of no dupplicates. 0 in case of duplicate
-	pop_push(&stack_a, &parse_list);
+		iterator = (*parse_list)->next;
+		while (iterator)
+		{
+			if (!comparison_values(&parse_list, &iterator))
+				return (0);
+			iterator = iterator->next;
+		}
+		pop_push(&stack_a, &parse_list);
+	}
+	return (1);
 }
