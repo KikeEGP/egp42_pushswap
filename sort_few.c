796b6400e75c9e0562e9d2460b04cf574e8f7420
42 enrgil-p <enrgil-p@c2r11s1.42madrid.com>
2025-03-26 21:30:03 +0100

    sort_few-> sort_three just need movements be created to test it. Conditions NOW ARE COMPLETELY ready

--- sort_few.c
+++ sort_few.c
@@ -6,26 +6,22 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 20:31:24 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/03/25 20:09:56 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/03/26 21:28:46 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+//With recursion, execution must pass two times unless case of 1st condition 
 void	sort_three(t_stack **stack_a, int size)
 {
-	ft_printf("\n\ninside sort three\n\n");
-	ft_printf("inside stack_siez == %d\n");
-	{
-		if (size == 2/*||pos 2 == is in 2th of list*/)
-			ft_printf("\n\nFirst node is %d\n", (*stack_a)->position);//debug
-			//sa();
-/*		else if (stack_a->position == 2)
-			ra();
-*/		else
-			ft_printf("Size is not 2, is %d\n", size);//debug
-/*			rra();
-*/	}
+	if (size == 2 || ((*stack_a)->position == 1 
+		&& (*stack_a)->next->position == 0))
+		sa();//creta sa() to try this
+	else if ((*stack_a)->position == 2)
+		ra();//create ra() to try this
+	else
+		rra();//same as above
 }
 /*Place to create functions for arguments <= 3, 4 or 5. 
  *
