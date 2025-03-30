237c874110a7efa0eaafc2f382f142eb127244e1
42 enrgil-p <enrgil-p@c2r17s1.42madrid.com>
2025-03-30 17:51:35 +0200

    Removed printfs of debug

--- error_clean.c
+++ error_clean.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 23:20:18 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/03/24 19:21:20 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/03/30 17:51:16 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,29 @@
 
 void	free_split(char **array)
 {
-	ft_printf("%p SPLIT before free\n", array);//debug
 	size_t	i;
 
 	i = 0;
 	while (array[i])
 		free(array[i++]);
 	free(array);
-	ft_printf("%p SPLIT free\n", array);//debug
 }
 
 void	clean_list(t_stack *head)
 {
 	t_stack	*keeper;
 
-	ft_printf("Inside of clean_list with %p\n", head);//debug
 	while (head)
 	{
-		ft_printf("Value is %d\n", head->value);//debug
-		ft_printf("Position is %d\n", head->position);//debug
 		keeper = head;
 		head = head->next;
 		free(keeper);
-	ft_printf("Node %p is free\n", head);//debug
 	}
 	//free(head);
 }
 
 void	free_stacks(t_stack *stack_a, t_stack *stack_b)
 {
-	ft_printf("Clean stacks\n");
 	if (stack_b)
 		clean_list(stack_b);
 	if (stack_a)
