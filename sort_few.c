d521f4b09f718dec723b055f068be690ff8f8dda
Enrique Gil Piqueras <enrgil-p@c1r9s3.42madrid.com>
2025-04-22 21:25:55 +0200

    Changes here must have been some debug printfs, I believe I removed it

--- sort_few.c
+++ sort_few.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 20:31:24 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/04/21 17:57:56 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/04/22 21:14:21 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,18 @@
 //With recursion, execution must pass two times unless case of 1st condition 
 void	sort_three(t_stack **stack_a)
 {
-//	t_stack	**last;
+	t_stack	**last;
 
+	last = NULL;
+	last = &(*stack_a)->next->next;
 	if (((*stack_a)->position == 1 
 		&& (*stack_a)->next->position == 0))
 		swap(&stack_a, 'a');//creta sa() to try this
-				    /*
-	**last = (*stack_a)->next->next;
 	else if ((*stack_a)->position == 2)
 		rotate(&stack_a, &last, 'a');//create ra() to try this
-	else
+/*	else
 		rra();//same as above
-*/
-}
+*/}
 /*Place to create functions for arguments <= 3, 4 or 5. 
  *
  * Remember, max 2 movs with 3 values. 12 moves max in cas of 5*/
