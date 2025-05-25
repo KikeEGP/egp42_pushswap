db2b33ac516f0114a1f6210965f90c44279bd5b3
Enrique Gil Piqueras <enrgil-p@c2r18s1.42madrid.com>
2025-05-25 16:49:56 +0200

    Created a intialize_sort_data function. I want to test it first to be sure that it will modify my stacks well

--- sort_large_input.c
+++ sort_large_input.c
@@ -6,17 +6,35 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 21:52:32 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/05/25 00:52:57 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/05/25 16:48:30 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	push_lower_values(/*Struct pointers*/, /*Struct quartiles*/)
+static int	initialize_sort_data(t_sort_data **data,
+		t_stack **st_a, t_stack **st_b, t_stack **last_a)
 {
-	if (!next_lower(head_a) /*What about stack_b empty or just one node?
-	It's made seg.fault before*/)
-		swap_both(head_a, head_b);
+	*data = (t_sort_data *)malloc(sizeof(t_sort_data));
+	if (!*data)
+		return (0);
+	(*data)->q1 = 0;
+	(*data)->median = 0;
+	(*data)->q3 = 0;
+	(*data)->stack_a = *stack_a;
+	(*data)->stack_b = *stack_b;
+	(*data)->last_a = *last_a;
+	(*data)->last_b = NULL;
+	(*data)->size_a = 0;
+	(*data)->size_b = 0;
+	return (1);
+}
+
+//int	push_lower_values(/*Struct pointers*/, /*Struct quartiles*/)
+//{
+//	if (!next_lower(head_a) /*What about stack_b empty or just one node?
+//	It's made seg.fault before*/)
+/*		swap_both(head_a, head_b);
 	else if (head_b->next && next_lower(head_b))
 		swap(head_b, 'b');//If swap_both works, this line is not needed
 	if ((head_a > last_a) && (last_b > last_a))
@@ -37,24 +55,23 @@ int	push_lower_values(/*Struct pointers*/, /*Struct quartiles*/)
 	else
 		rotate || (rotate_both (if last_b > head_b));
 
-}
+}*/
 
-void	big_sort(t_stack **st_a, t_stack **st_b, t_stack **last_a, int size_a)
+int	big_sort(t_stack **st_a, t_stack **st_b, t_stack **last_a, int size_a)
 {
-	static int	quartile_1;
-	static int	median;
-	static int	quartile_3;//May create a struct for quartiles
-	int			current_size_b;
-	t_stack		*last_b;//Also create a struct for all the pointers
+	t_sort_data	*data;
 
+	if (!initialize_sort_data(&data, st_a, st_b, last_a))
+		return (0);
 	current_size_b = 0;
-	get_quartiles(size_a, &quartile_1, &median, &quartile_3);
-	while (current_size_b <= median)
-		current_size_b = push_lower_values(/*Struct*/);
-	while (current_size_b <= quartile_3)
+	get_quartiles(size_a, data);
+	//while (current_size_b <= median)
+	//	current_size_b = push_lower_values(/*Struct*/);
+	/*while (current_size_b <= quartile_3)
 	{
 	}
 	while ((size_a - current_size_b) > 3)
 	{
-	}
+	}*/
+	return (1);
 }
--- sort_large_utils.c
+++ sort_large_utils.c
@@ -6,15 +6,17 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 00:02:18 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/05/25 00:05:06 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/05/25 16:29:45 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	get_quartiles(int size_a, int *q1, int *q2, int *q3)
+void	get_quartiles(int size_a, t_sort_data *sort_data)
 {
-	*q1 = size_a / 4;
-	*q2 = size_a / 2;
-	*q3 = *q1 + *q2;
+	sort_data->q1 = size_a / 4;
+	sort_data->q1 = size_a / 4;
+	sort_data->median = size_a / 2;
+	sort_data->q1 = sort_data->q1 + sort_data->q2;
+	sort_data->size_a = size_a;
 }
