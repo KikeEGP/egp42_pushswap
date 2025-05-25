1d917d99e0a7a2911029068b4f8ca9a1bc84ab61
Enrique Gil Piqueras <enrgil-p@c2r14s2.42madrid.com>
2025-05-25 19:14:29 +0200

    Correction of vars

--- sort_large_utils.c
+++ sort_large_utils.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 00:02:18 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/05/25 17:09:37 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/05/25 19:13:43 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 void	get_quartiles(int size_a, t_sort_data *sort_data)
 {
 	sort_data->q1 = size_a / 4;
-	sort_data->q1 = size_a / 4;
 	sort_data->median = size_a / 2;
-	sort_data->q1 = sort_data->q1 + sort_data->median;
+	sort_data->q3 = sort_data->q1 + sort_data->median;
 	sort_data->size_a = size_a;
 }
