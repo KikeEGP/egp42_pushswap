533c853485f4417c128d4d481ba283b33fc74337
Enrique Gil Piqueras <enrgil-p@c2r14s2.42madrid.com>
2025-05-25 21:38:57 +0200

    Corrected var name

--- sort_large_comparison_utils.c
+++ sort_large_comparison_utils.c
@@ -6,13 +6,13 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 00:02:18 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/05/25 19:39:38 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/05/25 21:38:38 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	second_is_lower(t_stack *node_1, t_stack *node_1)
+int	second_is_lower(t_stack *node_1, t_stack *node_2)
 {
 	if ((node_1 != node_2) && (node_1->value > node_2->value))
 		return (1);
