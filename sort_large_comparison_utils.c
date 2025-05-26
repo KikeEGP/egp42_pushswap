5e930c1a13056193794c15e67e34667652d54470
Enrique Gil Piqueras <enrgil-p@c2r17s1.42madrid.com>
2025-05-26 19:38:46 +0200

    Created is_consecutive() function, not added to push_swap.h yet

--- sort_large_comparison_utils.c
+++ sort_large_comparison_utils.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 00:02:18 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/05/25 21:38:38 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/05/26 18:34:25 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,12 @@ int	second_is_lower(t_stack *node_1, t_stack *node_2)
 	if ((node_1 != node_2) && (node_1->value > node_2->value))
 		return (1);
 	return (0);
+}
+
+int	is_consecutive(t_stack *node_1, t_stack *node_2)
+{
+	if ((node_1->position - node_2->position == 1)
+		|| (node_1->position - node_2->position == -1))
+		return (1);
+	return (0);/*Do not split these, manage together*/
 }
