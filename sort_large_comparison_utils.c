f1236913b4a2bd50f06c0dcd0975339c16bcfb15
Enrique Gil Piqueras <enrgil-p@c2r17s1.42madrid.com>
2025-05-26 20:37:30 +0200

    Change second to 2nd in its file, but not in push_swap.h

--- sort_large_comparison_utils.c
+++ sort_large_comparison_utils.c
@@ -6,13 +6,13 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 00:02:18 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/05/26 18:34:25 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/05/26 20:36:54 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	second_is_lower(t_stack *node_1, t_stack *node_2)
+int	2nd_lower(t_stack *node_1, t_stack *node_2)
 {
 	if ((node_1 != node_2) && (node_1->value > node_2->value))
 		return (1);
