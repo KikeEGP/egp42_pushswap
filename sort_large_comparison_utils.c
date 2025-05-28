51d1ab3c92a10672df1f6cb596353675485a50c3
Enrique Gil Piqueras <enrgil-p@c2r9s1.42madrid.com>
2025-05-28 19:14:38 +0200

    Comment explaining what does is_2nd_lower()

--- sort_large_comparison_utils.c
+++ sort_large_comparison_utils.c
@@ -6,12 +6,12 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 00:02:18 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/05/28 19:01:09 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/05/28 19:13:46 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
+//Both nodes must exist, point different memory blocks, and 1st node > 2nd node
 int	is_2nd_lower(t_stack *node_1, t_stack *node_2)
 {
 	if (node_1 && node_2 && (node_1 != node_2)
