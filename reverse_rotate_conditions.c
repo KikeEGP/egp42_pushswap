2afc7b2f2a0b15872f0cdf9adb44b9bdb9556b1a
Enrique Gil Piqueras <enrgil-p@c3r8s1.42madrid.com>
2025-05-29 18:58:21 +0200

    Simplified conditions, logic use of is_2nd_lower() function

--- reverse_rotate_conditions.c
+++ reverse_rotate_conditions.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   conditions.c                                       :+:      :+:    :+:   */
+/*   reverse_rotate_conditions.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 20:32:36 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/05/28 19:19:11 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/05/29 18:44:20 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,25 @@ int	conditions_rotate_both(t_sort_data *sd)
 	return (0);
 }
 
+int	conditions_reverse_stack_a(t_sort_data *sd)
+{
+	if (is_2nd_lower(sd->stack_a, sd->last_a)
+		|| is_2nd_lower(sd->stack_a, sd->last_a->prev))
+		return (1);
+	return (0);
+}
+
+int	conditions_reverse_stack_b(t_sort_data *sd)
+{
+	if (is_2nd_lower(sd_sd->last_b, sd->stack_b)
+		|| is_2nd_lower(sd->last_b->prev, sd->stack_b))
+		return (1);
+	return (0);
+}
+
 int	conditions_reverese_both(t_sort_data *sd)
 {
-	if ((is_2nd_lower(sd->stack_a, sd->last_a)
-			|| is_2nd_lower(sd->stack_a, sd->last_a->prev))
-		&& (is_2nd_lower(sd_sd->last_b, sd->stack_b)
-			|| !is_2nd_lower(sd->last_b->prev, sd->stack_b)))
+	if (conditions_reverse_stack_a(sd) && conditions_reverse_stack_b(sd))
 		return (1);
 	return (0);
 }
