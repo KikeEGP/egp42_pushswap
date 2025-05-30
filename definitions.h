5023cc095a306e0307bd524958793ecf33654ac7
Enrique Gil Piqueras <enrgil-p@c2r8s1.42madrid.com>
2025-05-30 21:19:31 +0200

    Removed quartiles from t_sort_data

--- definitions.h
+++ definitions.h
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 20:31:36 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/05/25 17:04:52 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/05/30 20:38:37 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,11 @@ typedef struct s_stack
 
 typedef struct s_sort_data
 {
-	int	q1;
-	int	median;
-	int	q3;
 	t_stack		*stack_a;
 	t_stack		*stack_b;
 	t_stack		*last_a;
 	t_stack		*last_b;
-	int			size_a;
+	int			size_a;/*Do you need this????*/
 	int			size_b;
 }	t_sort_data;
 
