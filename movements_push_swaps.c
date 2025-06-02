db3f450271ab7754c6fa406f761bb4ab7a31937f
Enrique Gil Piqueras <enrgil-p@c2r10s2.42madrid.com>
2025-06-02 19:02:26 +0200

    Changed rule of swap both, so it can execute ss if b is null

--- movements_push_swaps.c
+++ movements_push_swaps.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 20:15:16 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/06/02 18:22:44 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/06/02 19:00:57 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,14 +71,14 @@ void	swap(t_stack **stack_swapped, int id_stack)
 void	swap_both(t_stack **st_a, t_stack **st_b)
 {
 	if ((st_a && next_lower(*st_a))
-		&& (*st_b && !next_lower(*st_b)))
+		&& (/**st_b && */!next_lower(*st_b)))
 		/*In this if, I used to check if st_b->next exists.
 		But I had to remove it. And I don't like it. Review this, pls*/
 	{
 		ft_printf("SWAP BOTH\n");//debug
-		if (st_a)
+		if (*st_a)
 			swap(st_a, 0);
-		if (st_b)
+		if (*st_b)
 			swap(st_b, 0);
 		ft_putendl("ss");
 	}
