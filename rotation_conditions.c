1f36f03b2a77a8d270f20c378018e08c5d283ec4
Enrique Gil Piqueras <enrgil-p@c2r12s1.42madrid.com>
2025-06-03 18:04:38 +0200

    Changed in conditions how do we access to pointers

--- rotation_conditions.c
+++ rotation_conditions.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 20:32:36 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/06/03 17:30:04 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/06/03 18:04:00 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,47 +20,35 @@ int	conditions_rotate_stack_a(t_sort_data *sd)
 	return (0);
 }
 
+/*In stack_a, rotate is for integers higher to be pushed yet*/
 int	conditions_rotate_both(t_sort_data *sd)
 {
-	t_stack	*head_b;
-
-	head_b = *sd->stack_b;
-	if (head_b->position > sd->size_a / 4 && conditions_rotate_stack_a(sd))
+	ft_printf("Conditions rotate_both\n");//debug
+	if ((*sd->stack_b)->position > sd->size_a / 4
+		&& conditions_rotate_stack_a(sd))
 		return (1);
 	return (0);
 }
 
 int	conditions_reverse_stack_a(t_sort_data *sd)
 {
-	t_stack	*stack_a;
-	t_stack	*last_a;
-
-	stack_a = *sd->stack_a;
-	last_a = *sd->last_a;
 	ft_printf("We are in conditions, ");//debug
 	ft_printf("last_A is %d\n", last_a->value);//debug
 	ft_printf("last_A->prev is %d\n", last_a->prev->value);//debug
 	ft_printf("stack_A is %d\n", stack_a->value);//debug
-	if (is_2nd_lower(stack_a, last_a)
-		|| is_2nd_lower(stack_a, last_a->prev))
+	if (is_2nd_lower(*sd->stack_a, *sd->last_a)
+		|| is_2nd_lower(*sd->stack_a, (*sd->last_a)->prev))
 		return (1);
 	return (0);
 }
 
 int	conditions_reverse_stack_b(t_sort_data *sd)
 {
-	t_stack	*stack_b;
-	t_stack	*last_b;
-
 	ft_printf("\n\tConditions reverse_stack_b\n");//debug
 	ft_printf("sd->stack_B is %p\n", *sd->stack_b);//debug
 	ft_printf("sd->last_B is %p\n", *sd->last_b);//debug
-	stack_b = *sd->stack_b;
-	last_b = *sd->last_b;
-	ft_printf("We are in conditions, ");//debug
-	ft_printf("last_B is %p\n", *last_b);//debug
-	if (is_2nd_lower(last_b, stack_b)
-		|| is_2nd_lower(last_b->prev, stack_b))
+	if (is_2nd_lower(*sd->last_b, *sd->stack_b)
+		|| is_2nd_lower((*sd->last_b)->prev, *sd->stack_b))
 		return (1);
 	return (0);
 }
