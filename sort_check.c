952985e1895fdbc275265f9e93ba247912329a00
Enrique Gil Piqueras <enrgil-p@c1r4s1.42madrid.com>
2025-06-09 21:07:41 +0200

    Chabged a name in sort_check, and clean comments

--- sort_check.c
+++ sort_check.c
@@ -6,49 +6,30 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 20:31:06 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/06/08 20:06:48 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/06/09 21:07:17 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/*Function that checks if every node is in expected position*/
-/*It could iterate and compare iteration index with t_stack->position*/
-/*And storage number of times a number is not in expected place*/
-/*Maybe, in case of few unexpected positions, 
- * could work with different instructions. But pdel-olm suggest to focus
- * in algorithm. In case of being well programmed, it should do this too*/
-/*May check if stack_b exists and have nodes?*/
-int	sort_check(t_stack *stack_a)
+int	sort_check(t_stack *stack)
 {
 	int		check;
 
-	if (!stack_a)
+	if (!stack)
 		return (0);
 	check = 1;
-	while (stack_a->next != NULL && check > 0)
+	while (stack->next != NULL && check > 0)
 	{
-		ft_printf("\tWe are in sort_check. Values compared now are ");//debug
-		ft_printf("%d and ", stack_a->value);//debug
-		ft_printf("%d\n", stack_a->next->value);//debug
-		if (next_lower(stack_a))
-		{
-			ft_printf("So, will have to sort now\n");//debug
+		if (next_lower(stack))
 			check = -1;
-		}
-		stack_a = stack_a->next;
-		ft_printf("\tSort_check is %d\n", check);//debug
+		stack = stack->next;
 	}
-	ft_printf("\t\tOUT OF SORT_CHECK\n");
 	return (check);
 }
 
 int	sort_check_worths(t_stack *head_asked, t_stack *last_asked)
 {
-	ft_printf("Head is %d\n", head_asked->position);//debug
-	ft_printf("Next is %d\n", head_asked->next->position);//debug
-	ft_printf("Last is %d\n", last_asked->position);//debug
-	ft_printf("Prev is %d\n", last_asked->prev->position);//debug
 	if (is_2nd_lower(head_asked->next, head_asked)
 		&& is_2nd_lower(last_asked, last_asked->prev)
 		&& sort_check(head_asked) == 1)
