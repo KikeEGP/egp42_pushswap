70aacb4e9e3140748027f7ac91a84a4a3d395f57
Enrique Gil Piqueras <enrgil-p@c1r10s4.42madrid.com>
2025-06-17 20:17:01 +0200

    Changes in ibft

--- libft/conversions.h
+++ libft/conversions.h
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 19:03:40 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/01/17 23:27:25 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/06/17 20:16:41 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <limits.h>
 
 int		ft_atoi_protected(const char *str, int *counter);
-long	ft_atol(const char *str);
+long	ft_atol(const char *str, int *counter);
 long	ft_strtol_10(const char *source_str, char **endptr);
 
 #endif
--- libft/ft_atol.c
+++ libft/ft_atol.c
@@ -6,19 +6,20 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 17:15:13 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/01/17 19:06:07 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/06/17 20:08:38 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "conversions.h"
 
-long	ft_atol(const char *str)
+long	ft_atol(const char *str, int *counter)
 {
 	long	result;
 	long	sign;
 
 	result = 0;
 	sign = 1;
+
 	while ((*str >= 9 && *str <= 13) || *str == 32)
 		str++;
 	if (*str == '-' || *str == '+')
@@ -26,7 +27,12 @@ long	ft_atol(const char *str)
 		sign = 1 - 2 * (*str == '-');
 		str++;
 	}
+	while (*str >= '0' && *(str + 1) != '\0')
+		str++;
 	while (*str >= '0' && *str <= '9')
+	{
 		result = 10 * result + (*str++ - '0');
+		(*counter)++;
+	}
 	return (result * sign);
 }
