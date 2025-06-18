e71b736d3fd08e24e4d2757de81a0dda67dccb89
Enrique Gil Piqueras <enrgil-p@c1r6s2.42madrid.com>
2025-06-18 18:19:57 +0200

    Sort now does not return nothing

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 19:24:20 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/06/18 18:15:13 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/06/18 18:19:04 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	sort(t_stack **stack_a, t_stack **last_a)
+static void	sort(t_stack **stack_a, t_stack **last_a)
 {
 	int	sort_check_result;
 	int	size_a;
@@ -22,9 +22,7 @@ static int	sort(t_stack **stack_a, t_stack **last_a)
 	sort_check_result = check_sort_and_size(*stack_a, &size_a);
 	while (sort_check_result <= 0)
 	{
-		if (sort_check_result == 0)
-			return (0);//COULD THIS HAPPEN? CHECK BEFORE EVALUATE
-		else if (size_a <= 3)
+		if (size_a <= 3)
 			sort_three(stack_a, last_a, NULL);
 		else if (size_a <= 5)
 			sort_five(stack_a, last_a);
@@ -32,7 +30,6 @@ static int	sort(t_stack **stack_a, t_stack **last_a)
 			big_sort(stack_a, &stack_b, last_a, size_a);
 		sort_check_result = sort_check(*stack_a);
 	}
-	return (1);
 }
 
 int	main(int argc, char *a_vector[])
